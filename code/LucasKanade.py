e18b14596072b9c9246ac43a2e249ea2fc1a27e8
Michael <michael5511b@gmail.com>
2019-10-19 21:50:37 -0400

    Second Commit.

--- code/LucasKanade.py
+++ code/LucasKanade.py
@@ -1,8 +1,9 @@
 import numpy as np
 from scipy.interpolate import RectBivariateSpline
 
-def LucasKanade(It, It1, rect, p0 = np.zeros(2)):
-	# Input: 
+
+def LucasKanade(It, It1, rect, p0=np.zeros(2)):
+	# Input:
 	#	It: template image
 	#	It1: Current image
 	#	rect: Current position of the car
@@ -10,42 +11,102 @@ def LucasKanade(It, It1, rect, p0 = np.zeros(2)):
 	#	p0: Initial movement vector [dp_x0, dp_y0]
 	# Output:
 	#	p: movement vector [dp_x, dp_y]
-	
-    # Put your implementation here
-    x1 = rect[0] 
-    y1 = rect[1]
-    x2 = rect[2]
-    y2 = rect[3]
-    
-    Iy, Ix = np.gradient(It1)
+	# Put your implementation here
+	x1 = rect[0]
+	y1 = rect[1]
+	x2 = rect[2]
+	y2 = rect[3]
 
-    px = p0[0]
-    py = p0[1]
+	rect_len_x = x2 - x1 + 1
+	rect_len_y = y2 - y1 + 1
 
-    while smth < thresh:
-    	x1_warped = x1 + px
-    	y1_warped = y1 + py
-    	x2_warped = x2 + px
-    	y2_warped = y2 + py
+	Iy, Ix = np.gradient(It1)
 
+	px = p0[0]
+	py = p0[1]
 
-		X = np.linspace(x1_warped, x2_warped, x2_warped - x1_warped)
-		Y = np.linspace(x1, x2, x2 - x1) 	
-
-    	X_w = np.linspace(x1_warped, x2_warped, x2_warped - x1_warped)
-    	Y_w = np.linspace(y1_warped, y2_warped, y2_warped - y1_warped)
-
-    	xx, yy = np.meshgrid(X, Y)
-    	flat = np.stack((np.flatten(xx), np.flatten(yy)), axis=-1)
-    	xx_w, yy_w = np.meshgrid(X, Y)
-    	flat_w = np.stack((np.flatten(xx_w), np.flatten(yy_w)), axis=-1)
-
-    	It1(flat_w)
+	delta_p = np.array([[1], [1]])
+	thresh = 0.1
 
 
+	while np.sqrt(delta_p[0] ** 2 + delta_p[1] ** 2) > thresh:
+		px = p0[0]
+		py = p0[1]
 
-    p = p0
-    return p
+		x1_warped = x1 + px
+		y1_warped = y1 + py
+		x2_warped = x2 + px
+		y2_warped = y2 + py
 
-if __name__ == '__main__':
-	LucasKanade()
+		# Mesh for full image
+		X = np.arange(0, It.shape[0], 1)
+		Y = np.arange(0, It.shape[1], 1)
+		XX, YY = np.meshgrid(X, Y)
+		flat_XX = np.ndarray.flatten(XX)
+		flat_YY = np.ndarray.flatten(YY)
+
+		# Mesh for original rectangle
+		x = np.linspace(x1, x2, rect_len_x)
+		y = np.linspace(y1, y2, rect_len_y)
+		xx, yy = np.meshgrid(x, y)
+		flat_xx = np.ndarray.flatten(xx)
+		flat_yy = np.ndarray.flatten(yy)
+
+		# Mesh for warped rectangle
+		x_w = np.linspace(x1_warped, x2_warped, rect_len_x)
+		y_w = np.linspace(y1_warped, y2_warped, rect_len_y)
+		xx_w, yy_w = np.meshgrid(x_w, y_w)
+		flat_xx_w = np.ndarray.flatten(xx_w)
+		flat_yy_w = np.ndarray.flatten(yy_w)
+
+
+
+		# Create RectBivariateSpline object
+		# given a rectangle, RectBivariateSpline will return an "Object"
+		# We can then give the object non-integer indices, and the object would spit out interpolated values
+
+		spline_template = RectBivariateSpline(X, Y, It)
+		# .ev is tricky, the x and y are flipped!
+		values_template = spline_template.ev(flat_yy, flat_xx)
+
+		# Warping the current image to look like the template
+		# thus we use the warped coordinates, look at those values of the warped coordinates at the current image
+		# and compare the error to the template image
+
+		# In the Baker_Simon paper:
+		# The goal of the Lucas-Kanade algorithm is to minimize the sum of squared error between two
+		# images, the template T and the image I warped back onto the "coordinate frame of the template"
+
+		# Σ [I(W(x; p)) - T(x)]^2
+		# > I is the current image
+		# > T is the template image
+		# > W(x; p) take the pixel x in the coordinate frame of the template T and and maps it to the sub-pixel location W(x; p)
+		# in the coordinate frame of image I. So W is representing "coordinates"
+
+		spline_current = RectBivariateSpline(X, Y, It1)
+		# This right here is I(W(x; p))
+		values_warped = spline_current.ev(flat_yy_w, flat_xx_w)
+
+		error = values_template - values_warped
+
+		spline_gradient_x = RectBivariateSpline(X, Y, Ix)
+		grad_x_w = spline_gradient_x.ev(flat_yy_w, flat_xx_w)
+		spline_gradient_y = RectBivariateSpline(X, Y, Iy)
+		grad_y_w = spline_gradient_y.ev(flat_yy_w, flat_xx_w)
+
+		grad_I = np.stack((grad_x_w, grad_y_w), axis=-1)
+
+		# The Jacobian is an identity matrix, since there is only translation in our case
+		jacobian = np.array([[1, 0], [0, 1]])
+
+		# The acquisition of delta_p is derived from the First Order Taylor Series
+		# linearization of the square error function, check the paper!
+		# @ is matrix multiplication, it's the equivalent of matmul()
+		hessian = (grad_I @ jacobian).T @ (grad_I @ jacobian)
+		delta_p = np.linalg.inv(hessian) @ (grad_I @ jacobian).T @ error
+
+		p0[0] = p0[0] + delta_p[0]
+		p0[1] = p0[1] + delta_p[1]
+
+	p = p0
+	return p
--- code/testCarSequence.py
+++ code/testCarSequence.py
@@ -2,5 +2,50 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import animation
 import matplotlib.patches as patches
+from LucasKanade import LucasKanade
 
 # write your script here, we recommend the above libraries for making your animation
+ims = []
+if __name__ == '__main__':
+    vid = np.load("../data/carseq.npy")
+    fig = plt.figure(1)
+    x, y, num_frame = vid.shape
+
+    rect = [59, 116, 145, 151]
+    rects = []
+    rects.append(rect)
+
+    for i in range(num_frame - 1):
+        frame_temp = vid[:, :, i]
+        frame_curr = vid[:, :, i + 1]
+        p = LucasKanade(frame_temp, frame_curr, rect, p0=np.zeros(2))
+
+        rect[0] = rect[0] + p[0]
+        rect[1] = rect[1] + p[1]
+        rect[2] = rect[2] + p[0]
+        rect[3] = rect[3] + p[1]
+
+        rects.append(rect)
+
+        # For display animation
+        """
+        r = patches.Rectangle((rect[0], rect[1]), rect[2] - rect[0] + 1, rect[3] - rect[1] + 1, linewidth=2, edgecolor='r', facecolor='none')
+        plt.imshow(frame_curr, cmap='gray')
+        plt.gca().add_patch(r)
+        plt.pause(0.001)
+        r.remove()
+        """
+
+        # For frames 1, 100, 300, 400
+        """
+        if i == 0 or i == 99 or i == 199 or i ==299 or i == 399:
+            r = patches.Rectangle((rect[0], rect[1]), rect[2] - rect[0] + 1, rect[3] - rect[1] + 1, linewidth=2, edgecolor='r', facecolor='none')
+            plt.imshow(frame_curr, cmap='gray')
+            plt.gca().add_patch(r)
+            plt.show()
+            plt.pause(5)
+            plt.close()
+            r.remove()
+        """
+    carseqrects = np.array(rects)
+    np.save("../outputs/carseqrects.npy", carseqrects)
